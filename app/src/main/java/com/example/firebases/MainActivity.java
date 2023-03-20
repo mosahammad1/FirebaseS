0870f7e62514c2ba7d42ce3452f382b7327e7d95
unknown <mosa.20hammad@gmail.com>
2023-03-20 03:53:05 +0200

    FComment

---
+++ app/src/main/java/com/example/firebases/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.firebases;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton floatingActionButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        floatingActionButton = findViewById(R.id.float_btn);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),UploadPDF.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
